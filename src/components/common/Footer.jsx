a03c52f1401f2c834cdf8b5f8d1e091fa6e6d692
Bruno Cordoba <brunonahuel87@gmail.com>
2024-03-06 15:07:42 -0300

    mejore el footer

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -4,14 +4,14 @@ import Col from 'react-bootstrap/Col';
 
 const Footer = () => {
     return (
-       <footer className='text-center  py-2 bg-primary text-white'>
-       <Container>
-        <Row  >
-            <Col md={6} lg={6} ><p>Hola mundo</p></Col>
-            <Col md={6} lg={6} ><p>Hola mundo</p></Col>
-        </Row>
-       </Container>
-       </footer>
+        <footer className='text-center py-2 bg-primary text-white'>
+            <Container>
+                <Row>
+                    <Col md={6} lg={6}><p>Recetas Bonapettit<br /> <i class="bi bi-map"></i>123 Calle Principal, Ciudad</p></Col>
+                    <Col md={6} lg={6}><p>Síguenos en redes:<br /><i class="bi bi-instagram"></i> @recetasbonapettit<br /><i class="bi bi-facebook"></i> Recetas Bonapettit</p></Col>
+                </Row>
+            </Container>
+        </footer>
     );
 };
 
